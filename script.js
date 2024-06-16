035894fea38c9943322f357ac719e88d6890c502
ribafs2 <162622666+ribafs2@users.noreply.github.com>
2024-06-16 16:32:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ setAlarmBtn = document.querySelector("button");
 let alarmTime, isAlarmSet,
 ringtone = new Audio("./files/ringtone.mp3");
 
-for (let i = 12; i >= 0; i--) {
+for (let i = 12; i > 0; i--) {
     i = i < 10 ? `0${i}` : i;
     let option = `<option value="${i}">${i}</option>`;
     selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
